17bbf15b441a647899de3d09f20d8a2948beb23e
Felipe <kendyhonda@hotmail.com>
2021-07-30 18:22:31 -0300

    Adicionando os arquivos da loja de jogos

---
+++ LojaDeGames/src/main/java/br/org/generation/lojadegames/repository/CategoriaRepository.java
@@ -0,0 +1,11 @@
+package br.org.generation.lojadegames.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import br.org.generation.lojadegames.model.Categoria;
+
+@Repository
+public interface CategoriaRepository extends JpaRepository<Categoria, Long> {
+
+}
